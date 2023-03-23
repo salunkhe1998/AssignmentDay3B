6f3714d5e9aa02e1f1543863a4611e7589f5f836
Ashwini <salunkheashwini1998@gmail.com>
2023-03-23 15:02:44 +0530

    This is file

---
+++ DefaultConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay3B
+{
+    //internal class DefaultConstructor
+    class Geek
+    {
+
+        int num;
+        string name;
+        Geek()
+        {
+            Console.WriteLine("Constructor Called");
+        }
+ 
+    
+    }
+}
